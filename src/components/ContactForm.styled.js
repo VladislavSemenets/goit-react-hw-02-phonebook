6bd99673ba49f3d3d4dfab063dc8ddcce87e4b23
VladislavSemenets <124085061+VladislavSemenets@users.noreply.github.com>
2023-09-04 21:29:36 +0300

    recreate without issues

---
+++ src/components/ContactForm.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Label = styled.label `
+
+`;
+
+export const Form = styled.form `
+
+`
+
+export const Btn = styled.button `
+margin-top: 5px;
+`
